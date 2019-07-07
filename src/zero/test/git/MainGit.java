2b1b474e0b7e6b05791de658e9a015fe95f497df
wangdongdong <827495316@qq.com>
2019-07-07 15:51:03 +0800

    Initial commit

---
+++ src/zero/test/git/MainGit.java
@@ -0,0 +1,13 @@
+package zero.test.git;
+
+/**
+ * @program: javabased
+ * @description: ${description}
+ * @author: Mr.Wang
+ * @create: 2019-07-07 15:44
+ **/
+public class MainGit {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
